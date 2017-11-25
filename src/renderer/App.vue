ddd28a7274867a06989d5ac51a54a18825df0a6b
ssthouse <ssthouse@163.com>
2017-11-25 10:52:39 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+        <div class="row">
+            <div class="col-lg-4">1</div>
+            <div class="col-lg-4">1</div>
+            <div class="col-lg-4">1</div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'ssthouse-tool'
+  }
+</script>
+
+<style>
+    /* CSS */
+</style>
